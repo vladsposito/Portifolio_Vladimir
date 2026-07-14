cf1593778b4afbbfa3402d5206ae8917c79b70ad
Vlad <vladlealoliv@gmail.com>
2026-07-14 12:37:14 -0300

    header style tablet e atulizando header style mobile

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const menuHamburguer = document.querySelector("#menu-hamburguer")
 
 const navigationList = document.querySelector(".navigation-menu ul")
 
+
 menuHamburguer.addEventListener("click", ()=>{
     navigationList.classList.toggle("active")
 })
