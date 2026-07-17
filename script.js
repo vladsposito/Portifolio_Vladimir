f65a8a37d578db0aa51238cd4541e3192f0bf2b8
Vlad <vladlealoliv@gmail.com>
2026-07-17 16:24:44 -0300

    scroll e observer

--- script.js
+++ script.js
@@ -4,17 +4,60 @@ const navigationList = document.querySelector(".navigation-menu ul")
 
 const navigationItem = document.querySelectorAll(".navigation-item")
 
+const sections = document.querySelectorAll("section")
+
 //Menu Hamburguer
 menuHamburguer.addEventListener("click", ()=>{
     navigationList.classList.toggle("active")
 })
 
+
 //Navigation Menu Border
-navigationItem.forEach(element => {
+navigationItem.forEach((element) => { 
     element.addEventListener("click", ()=>{
+        event.preventDefault()
         navigationItem.forEach(element => {
             element.classList.remove("active")
         });
         element.classList.add("active")
     })
-});
+})
+
+
+//Scroll to Section
+    function scroll(menuIndex, sectionIndex){
+        navigationItem[menuIndex].addEventListener("click", ()=>{
+            sections[sectionIndex].scrollIntoView({
+                behavior: "smooth"
+            })
+        })
+    }
+    scroll(0, 0)
+    scroll(1, 1)
+    scroll(2, 2)
+    scroll(3, 3)
+
+
+//Obersrver
+const observer = new IntersectionObserver((entries) =>{ //entries é o array que recebe os elementos "olhados" pelo observer
+    entries.forEach(entry => { //entry representa cada objeto do entries
+        if(entry.isIntersecting){ //isIntersecting é um boolean para dizer se o objeto esta sendo visto na tela
+            navigationItem.forEach(element => {
+                element.classList.remove("active") 
+                //retirando os active antigos para entrar os novos
+                
+            });
+
+            const index = [...sections].indexOf(entry.target)
+            //estraindo o indice da secao que está no campo de visão
+            //indexOf estrai o indice e entry.target seleciona o elemento que esta no campo de visão
+
+            navigationItem[index].classList.add("active")
+        }
+        
+       
+    });
+     threshold: 0.5
+})
+
+sections.forEach(sections => observer.observe(sections));
