37170c32823122a410a77b2a507295c56eb61b90
Vlad <vladlealoliv@gmail.com>
2026-07-17 11:51:15 -0300

    nav menu border animation

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ const menuHamburguer = document.querySelector("#menu-hamburguer")
 
 const navigationList = document.querySelector(".navigation-menu ul")
 
+const navigationItem = document.querySelectorAll(".navigation-item")
 
 menuHamburguer.addEventListener("click", ()=>{
     navigationList.classList.toggle("active")
 })
+
+navigationItem.forEach(element => {
+    element.addEventListener("click", ()=>{
+        element.classList.toggle("active")
+    })
+});
